65b4be72f473d216cb43314b28f17ea7f4256c5a
jknaudt21 <jknaudt21@cmc.edu>
2020-11-21 16:52:18 -0400

    update clean.py

--- melanoma_segmentation_Unet/clean.py
+++ melanoma_segmentation_Unet/clean.py
@@ -3,8 +3,10 @@
 # Import depedencies
 import os
 
-for file in os.listdir('./Training'):
-    if file.endswith('.png'): # png files in the training data are not needed, so we remove the m
-        os.remove('./Training/'+file)
+for file in os.listdir('./data/Training'):
+    if file.endswith('.png') or file.endswith('.csv'): # png and csv files in the training data are not needed
+        os.remove('./data/Training/'+file)
 
-
+for file in os.listdir('./data/Masks'):
+    if file.endswith('.csv'): # csv files in the validation data are not needed
+        os.remove('./data/Masks/'+file)
